d46504ab46039b5e516a9848d979679c8f767496
SatishK2022 <sk7399052@gmail.com>
2024-01-27 12:35:55 +0530

    utils code and middlewares

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,12 +1,31 @@
 // require('dotenv').config({path: './env'})
 import dotenv from 'dotenv'
 import connectDB from './db/index.js';
+import { app } from './app.js'
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("Error ", error)
+            process.exit(1)
+        })
+
+        app.listen(process.env.PORT || 5001, () => {
+            console.log(`App listening on http://localhost:${process.env.PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log("MONGO DB Connection Failed !!", error)
+    })
+
+
+
+
+
 
 
 
